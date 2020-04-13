40be2568fd0e1c33f1d0bb5a90186bbfa9eb3d96
SamPhung <45484385+SamPhung@users.noreply.github.com>
2020-04-13 19:53:41 +0700

    initial project

---
+++ Assets/Scrip/BGCloud/Cloud1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud1 : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _CloudMovement();
+    }
+    void _CloudMovement()
+    {
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
---
+++ Assets/Scrip/BGSpawnerCloud/BGSpawnerCloud1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGSpawnerCloud1 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cloud;
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+
+    IEnumerator Spawner() //Deplay để sinh ra
+    {
+        yield return new WaitForSeconds(17);
+        //Vector3 temp = cloud.transform.position;
+        //temp.y = Random.Range(-2.5f, 2.5f);
+        Instantiate(cloud, transform.position, Quaternion.identity);
+        StartCoroutine(Spawner());
+    }
+}
